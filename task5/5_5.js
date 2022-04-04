090a604c6639b44e0dbd454b4a45b4738bb25abb
Den <bikerden69@tut.by>
2022-04-04 23:07:32 +0300

    fix: up to down

--- task5/5_5.js
+++ task5/5_5.js
@@ -1,9 +1,6 @@
 //5. Отсортировать массив [1,6,7,8,3,4,5,6] по убыванию
 let arrayForSort = [1, 6, 7, 8, 3, 4, 5, 6];
-
-let resOFSort = arrayForSort.sort().reverse(); //simply :-)
-let resOFSortV2 = arrayForSort.sort((a, b) => a - b);
-
 console.log("🚀 initial array is", arrayForSort);
-console.log("🚀 resOFSort", resOFSort);
-console.log("🚀 resOFSortV2", resOFSortV2);
+
+console.log("🚀 resOFSortV1", arrayForSort.sort().reverse());
+console.log("🚀 resOFSortV2", arrayForSort.sort((a, b) => b - a));
